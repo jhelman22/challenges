433bf65bcb71abaf424dd7d745ad5d85c72b228d
Jake Helman <jacob.helman@hbdi.com>
2017-06-02 07:25:41 -0400

    reddit daily programmer challenge 1-easy

---
+++ reddit_dailyprogrammer/001_easy.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+puts "What's your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What's your reddit username?"
+reddit = gets.chomp
+puts "\nYour name is #{name}, you are #{age} years old, and your username is #{reddit}"
